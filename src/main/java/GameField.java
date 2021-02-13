73ec70896cac0b91e4bf1e8b0f415b600b200f56
demiurg <john-alp@mail.ru>
2021-02-13 12:57:36 +0300

    clean

--- src/main/java/GameField.java
+++ src/main/java/GameField.java
@@ -18,115 +18,116 @@ public class GameField extends JPanel implements ActionListener {
     private int[] y = new int[ALL_DOTS];
     private int dots;  // размер змейки в текущий момент
     private Timer timer; // стандартный свинговый таймер
-    private  boolean left = false; // начальное значение false
+    private boolean left = false; // начальное значение false
     private boolean right = true;
     private boolean up = false;
     private boolean down = false;
     private boolean inGame = true; //
-    /*
-      Конструктор
-     */
-    public GameField(){
-            setBackground(Color.black);
-            loadImages();
-            initGame();
-            addKeyListener(new FieldKeyListener());
-            setFocusable(true);
-        }
-        public void initGame(){
+
+    public GameField() {
+        setBackground(Color.black);
+        loadImages();
+        initGame();
+        addKeyListener(new FieldKeyListener());
+        setFocusable(true);
+    }
+
+    public void initGame() {
         dots = 3; // Размер змейки 3x16
-        for (int i = 0; i < dots; i++){
-            x[i] = 48 - i*DOT_SIZE; // DOT_SIZE размер точки .. 1/x=48 2/x=32 3/x=16
+        for (int i = 0; i < dots; i++) {
+            x[i] = 48 - i * DOT_SIZE; // DOT_SIZE размер точки .. 1/x=48 2/x=32 3/x=16
             y[i] = 48;
         }
-        timer = new Timer(250,this); // 250 миллисекунд, на классе GameField
+        timer = new Timer(250, this); // 250 миллисекунд, на классе GameField
         timer.start();
         createApple();
+    }
 
-        }
+    public void createApple() {
+        appleX = new Random().nextInt(20) * DOT_SIZE; // на игровое поле может поместиться 20, 16 пиксельных квадратиков
+        appleY = new Random().nextInt(20) * DOT_SIZE; // от 0 до 19
+    }
 
-        public void createApple(){
-        appleX = new Random().nextInt(20)*DOT_SIZE; // на игровое поле может поместиться 20, 16 пиксельных квадратиков
-        appleY = new Random().nextInt(20)*DOT_SIZE; // от 0 до 19
-        }
+    public void loadImages() {  // загружаем 16 битные картинки
+        ImageIcon imageIconApple = new ImageIcon("apple.png");
+        apple = imageIconApple.getImage();
+        ImageIcon imageIconDot = new ImageIcon("dot.png");
+        dot = imageIconDot.getImage();
+    }
 
-        public void loadImages(){  // загружаем 16 битные картинки
-            ImageIcon imageIconApple = new ImageIcon("apple.png");
-            apple = imageIconApple.getImage();
-            ImageIcon imageIconDot = new ImageIcon("dot.png");
-            dot = imageIconDot.getImage();
-        }
-
-       @Override  // перерисовываем змейку
-       protected void paintComponent(Graphics g){
+    @Override  // перерисовываем змейку
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (inGame){
-            g.drawImage(apple,appleX,appleY,this);
-            for (int i = 0; i < dots ; i++) {   // тело змейки
-                g.drawImage(dot,x[i],y[i],this);
+        if (inGame) {
+            g.drawImage(apple, appleX, appleY, this);
+            for (int i = 0; i < dots; i++) {   // тело змейки
+                g.drawImage(dot, x[i], y[i], this);
             }
-        }
-        else{
-            String str = "Game Over";
-         //   Font font = new Font("Arial",14,Font.BOLD);
+        } else {
+            String str = " Game Over ";
+            //   Font font = new Font("Arial",14,Font.BOLD);
             g.setColor(Color.ORANGE);
-         //   g.setFont(font);
-            g.drawString(str,125,SIZE/2);
+            //   g.setFont(font);
+            g.drawString(str, 125, SIZE / 2);
         }
-       }
+    }
 
-        // будем проводить логическую перерисовку точки, тоесть делать сдвиг в массиве
-        public void move(){
-            for (int i = dots; i > 0 ; i--) {
-                x[i] = x[i - 1]; // сдвигаем тело змейки
-                y[i] = y[i - 1];
-            }
-         if (left){    // а голову передвигаем в нужном направлении
+    // проводим логическую перерисовку точки, тоесть делаем сдвиг в массиве
+    public void move() {
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i - 1]; // сдвигаем тело змейки
+            y[i] = y[i - 1];
+        }
+        if (left) {    // а голову передвигаем в нужном направлении
             x[0] -= DOT_SIZE;
         }
-         if (right){
+        if (right) {
             x[0] += DOT_SIZE;
-         }
-         if (up){
-             y[0] -= DOT_SIZE;
-         }
-         if (down){
-             y[0] += DOT_SIZE;
-         }
         }
-    public void checkApple(){
-        if(x[0] == appleX && y[0] == appleY){
+        if (up) {
+            y[0] -= DOT_SIZE;
+        }
+        if (down) {
+            y[0] += DOT_SIZE;
+        }
+    }
+
+    public void checkApple() {
+        if (x[0] == appleX && y[0] == appleY) {
             dots++;
             createApple();
         }
     }
-    public void checkCollisions(){
-        for (int i = dots; i > 0 ; i--) {  // столкновение со своим телом
-            if(i > 4 && x[0] == x[i] && y[0] == y[i])  // если тело > 4, координаты головы совподают с координатами тела змейки
+
+    public void checkCollisions() {
+        for (int i = dots; i > 0; i--) {  // столкновение со своим телом
+            if (i > 4 && x[0] == x[i] && y[0] == y[i])  // если тело > 4, координаты головы совподают с координатами тела змейки
                 inGame = false;
         }
-        if(x[0] > SIZE){
+        if (x[0] > SIZE) {
             inGame = false;
         }
-        if(x[0] < 0){
-            inGame =false;
-        }
-        if(y[0] > SIZE){
+        if (x[0] < 0) {
             inGame = false;
         }
-        if(y[0] < 0){
+        if (y[0] > SIZE) {
+            inGame = false;
+        }
+        if (y[0] < 0) {
             inGame = false;
         }
     }
+
     @Override // вызывается каждый раз, когда "тикает" таймер
     public void actionPerformed(ActionEvent e) {
-         if (inGame){   // inGame - если мы в игре
-             checkApple();
-             checkCollisions();
-             move();
-         }
-         repaint();
+        if (inGame) {   // inGame - если мы в игре
+            checkApple();
+            checkCollisions();
+            move();
+        }
+        repaint();
     }
+
     /*
      KeyListener обрабатыват события с клавиатуры
     KeyListener имеет три метода: keyTyped, keyPressed и keyReleased.
@@ -137,33 +138,32 @@ public class GameField extends JPanel implements ActionListener {
     используется метод addKeyListener. В качестве параметра методу передается ссылка на слушателя.
       Для удаления слушателя используется метод removeKeyListener.
      */
-    class FieldKeyListener extends KeyAdapter{
+    class FieldKeyListener extends KeyAdapter {
         @Override
-        public void keyPressed(KeyEvent e){
+        public void keyPressed(KeyEvent e) {
             super.keyPressed(e);
             int key = e.getKeyCode();
             System.out.println(key);
-            if(key == KeyEvent.VK_LEFT && !right){
+            if (key == KeyEvent.VK_LEFT && !right) {
                 left = true;
                 up = false;
                 down = false;
             }
-            if(key == KeyEvent.VK_RIGHT && !left){
+            if (key == KeyEvent.VK_RIGHT && !left) {
                 right = true;
                 up = false;
                 down = false;
             }
-            if(key == KeyEvent.VK_UP && !down){
+            if (key == KeyEvent.VK_UP && !down) {
                 right = false;
                 up = true;
                 left = false;
             }
-            if(key == KeyEvent.VK_DOWN && !up){
+            if (key == KeyEvent.VK_DOWN && !up) {
                 right = false;
                 down = true;
                 left = false;
             }
         }
-
     }
 }
--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -2,14 +2,13 @@ import javax.swing.*;
 
 public class MainWindow extends JFrame {
     // создаем конструктор и описывем свойства
-    public MainWindow(){
+    public MainWindow() {
         setTitle("Super Snake");
         setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE); // при нажатии крестика на окне, заершаем работу приложения
-        setSize(330,355); // размер окна
-        setLocation(500,200); // позиция окна?
+        setSize(330, 355); // размер окна
+        setLocation(500, 200); // позиция окна
         add(new GameField());
         setVisible(true);
-
     }
 
     public static void main(String[] args) {
